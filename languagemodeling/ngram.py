4370a14a8c45855c1b9d8beb30a578a6138d1b1c
Anto <antonella@theamalgama.com>
2018-02-10 12:05:52 -0300

    Resolution of ngram model - all test_ngram ok
    - Implementation of ngram model: completed constructor, count, cond_prob, sent_prob and sent_log_prob of class.
    - All tests from test_ngram working.

--- languagemodeling/ngram.py
+++ languagemodeling/ngram.py
@@ -50,35 +50,81 @@ class NGram(LanguageModel):
 
         count = defaultdict(int)
 
-        # WORK HERE!!
+        for sent in sents:
+            self.addDelimiterToSentence(sent)
+            self.updateCountOfSentenceWithNgram( count, sent, n )
+            self.updateCountOfSentenceWithNgram( count, sent, n - 1 )
 
         self._count = dict(count)
+
+    def updateCountOfSentenceWithNgram(self, count, sent, n):
+        maximumStartOfNgram = len(sent) - ( n - 1)
+        for i in range( min( maximumStartOfNgram, len(sent) ) ):
+            ngram = tuple( sent[i:i+n] )
+            count[ngram] += 1
+
+    def addDelimiterToSentence(self, sent):
+        for i in range( self._n - 1 ):
+            sent.insert(0, '<s>')
+        sent.append('</s>')
 
     def count(self, tokens):
         """Count for an n-gram or (n-1)-gram.
 
         tokens -- the n-gram or (n-1)-gram tuple.
         """
-        return self._count.get(tokens, 0)
+        return self._count.get( tuple(tokens), 0 )
 
-    def cond_prob(self, token, prev_tokens=None):
+    def cond_prob(self, token, prev_tokens=[]):
         """Conditional probability of a token.
 
         token -- the token.
         prev_tokens -- the previous n-1 tokens (optional only if n = 1).
         """
-        # WORK HERE!!
+        if self.count(prev_tokens) == 0:
+            return 0
+        else:
+            whole_sentence_as_list = ( prev_tokens + [token] ) if ( isinstance(prev_tokens, list) ) else list( prev_tokens + (token,) )
+            return self.count( whole_sentence_as_list ) / self.count( prev_tokens )
 
     def sent_prob(self, sent):
         """Probability of a sentence. Warning: subject to underflow problems.
 
         sent -- the sentence as a list of tokens.
         """
-        # WORK HERE!!
+        self.addDelimiterToSentence(sent)
+        prob = 1
+
+        for i in range(len(sent) - self._n + 1):
+            ngramSent = sent[i:i+self._n]
+            prob *= self.cond_prob_of_sentence(ngramSent)
+            
+        return prob
 
     def sent_log_prob(self, sent):
         """Log-probability of a sentence.
 
         sent -- the sentence as a list of tokens.
         """
-        # WORK HERE!!
+        self.addDelimiterToSentence(sent)
+        prob = 0
+
+        for i in range(len(sent) - self._n + 1):
+            ngramSent = sent[i:i+self._n]
+            cond_prob = self.cond_prob_of_sentence(ngramSent)
+
+            if cond_prob != 0:
+                prob += math.log( cond_prob, 2)
+            else:
+                return -float('inf')
+
+        return prob
+
+    def cond_prob_of_sentence(self, sentence):
+        assert len(sentence) >= self._n
+
+        if (self._n > 1):
+            return self.cond_prob( sentence[self._n-1], sentence[0:self._n-1] )
+        else:
+            return self.cond_prob( sentence[self._n-1] )
+

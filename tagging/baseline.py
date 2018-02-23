4e11a453b82e6dc89776a6897c2bd69a75b78e35
Anto <antonella@theamalgama.com>
2018-02-23 19:52:42 -0300

    Implementation of baseline tagger

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -37,7 +37,20 @@ class BaselineTagger:
         """
         tagged_sents -- training sentences, each one being a list of pairs.
         """
-        # WORK HERE!!
+        word_tags_count = dict()
+        tag_counts = defaultdict(int)
+        for tagged_sent in tagged_sents:
+            for word_and_tag in tagged_sent:
+                word = word_and_tag[0]
+                tag = word_and_tag[1]
+                tag_counts[tag] += 1
+                word_tags_count.setdefault(word, defaultdict(int))
+                word_tags_count[word][tag] += 1
+
+        self._most_common_tag = sorted(tag_counts.items(), key=lambda x: -x[1])[0][0]
+        self._word_tags_count = dict()
+        for word, tags_dict in word_tags_count.items():
+            self._word_tags_count[word] = sorted(tags_dict.items(), key=lambda x: -x[1])
 
     def tag(self, sent):
         """Tag a sentence.
@@ -51,11 +64,13 @@ class BaselineTagger:
 
         w -- the word.
         """
-        # WORK HERE!!
+        if self.unknown(w):
+            return self._most_common_tag
+        return self._word_tags_count.get(w)[0][0]
 
     def unknown(self, w):
         """Check if a word is unknown for the model.
 
         w -- the word.
         """
-        # WORK HERE!!
+        return w not in self._word_tags_count.keys()
